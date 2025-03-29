7d5fe11a281342cf0c810a3ec4e004e3c3f1d036
Anna Ruggeri <ruggeriannaclara@gmail.com>
2025-03-29 16:19:59 -0300

    Add files via upload

---
+++ Web Development/script.js
@@ -0,0 +1,8 @@
+var nome= prompt("Digite seu nome:");
+alert("Olá, " + nome + "!");
+var idade = prompt("Digite sua idade: ");
+
+var num1 = parseFloat(prompt("DIgite um número: "));
+var num2 = parseFloat(prompt("Digite outro número: "));
+var soma = num1 + num2;
+alert("A soma dos números é: " + soma);
